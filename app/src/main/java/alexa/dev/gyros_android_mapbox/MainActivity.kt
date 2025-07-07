17fc81e10c16e653bdd57e1f549340d17811f132
alexexisted <a.assjones@list.ru>
2025-07-07 18:10:42 +0200

    DI dagger hilt setup

--- app/src/main/java/alexa/dev/gyros_android_mapbox/MainActivity.kt
+++ app/src/main/java/alexa/dev/gyros_android_mapbox/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import alexa.dev.gyros_android_mapbox.ui.theme.GyrosandroidmapboxTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
